ca92d83288a1c89a085ada0ce00d49d7a8ed625e
VaishnaviRaoB <vaishnaviraob61@gmail.com>
2024-12-12 20:53:35 +0530

    New Styling

--- NBAcademix/NBAcademix/settings.py
+++ NBAcademix/NBAcademix/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'adminpage' / 'static',  # Adjust this to your project structure
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
